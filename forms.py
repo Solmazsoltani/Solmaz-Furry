6c123ffd1ba96ccfef58f85bc90fb06850a01b42
Solmazsoltani <88332157+Solmazsoltani@users.noreply.github.com>
2022-05-11 18:34:07 +0200

    Add files via upload

--- forms.py
+++ forms.py
@@ -15,23 +15,38 @@ from sqlalchemy import false, null
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, AnyOf, URL,Regexp,re,length,Optional
 from phonenumbers import ValidationResult, timezone
-
-
+from enums import Genre, State
+from wtforms.fields import TelField
+'''
 def validate_website(form,field):
-  url_regex = '((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*'
-  regex = re.compile(url_regex)
-  if not re.match(field,regex):
-      raise ValidationErr('The correct format for the website link was not correct')
-      
-def validate_phone(form, phone):
-        try:
-            p = phonenumbers.parse(phone.data)
-            if not phonenumbers.is_valid_number(p):
-                raise ValueError()
-        except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
-            raise ValidationErr('Invalid phone number')
+    url_regex = '((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*'
+    regex1 = re.compile(url_regex)
+    try:
 
-       
+     if not regex1.match(field.data):
+        raise ValidationErr('The correct format for the website link was not correct')
+    except :
+            raise ValidationErr('Invalid phone numberzjgj ')
+            flash('Invalid phone number')
+    finally:
+           return 
+'''
+
+def is_valid_phone(number):
+    """ Validate phone numbers like:
+    1234567890 - no space
+    123.456.7890 - dot separator
+    123-456-7890 - dash separator
+    123 456 7890 - space separator
+    Patterns:
+    000 = [0-9]{3}
+    0000 = [0-9]{4}
+    -.  = ?[-. ]
+    Note: (? = optional) - Learn more: https://regex101.com/
+    """
+    regex = re.compile('^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$')
+    return regex.match(number)
+
 class ShowForm(Form):
     artist_id = StringField(
         'artist_id'
@@ -44,111 +59,26 @@ class ShowForm(Form):
         validators=[DataRequired()],
         default= datetime.today()
     )
-#@app.route('/venues/create', methods=['POST'])
-#def create_venue_submission():
- # error = False
- # form = VenueForm()
- # if form.validate_on_submit():
-   # try:
-   #   name = request.form['name']
-  #    city = request.form['city']
-  #   ...
-   # except:
-  #    error = True
-   #   db.session.rollback()
-  #  finally:
-  #    db.session.close()
- # else:
-  #  for error in form.errors:
-   #   flash(error)
-  #return render_template('forms/new_venue.html', form=form)
-  #
-   
-
 class VenueForm(Form):
-    genres_choices= [
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
-        
-    #def validate_phone(form, field):
-        #if not re.search(r"^[0-9]{3}-[0-9]{3}-[0-9]{4}$", field.data):
-            #raise ValidationErr("Invalid phone number.")
-
-
-   # def validate_genres(form, field):
-        
-        #  genres_values = [choice[1] for choice in field]
-        #  for value in field.data:
-        #  if value not in genres_values:
-        #  raise ValidationErr('Invalid genres value.')
-
-
-    
-
+            
     name = StringField('Name', [DataRequired()])
+    
     def validate_name(form, field):
-                if len(field.data)==3:
-                 raise ValidationErr('Name must be less than 50 characters')
-                 
-    
-    
+        try:                                                                                                              
+            if len(field.data)==3:
+                 raise ValidationErr('Name has three characters')
+        except :
+            flash('Invalid phone number')
+        finally:
+           return 
+        
     city = StringField(
         'city', validators=[DataRequired()]
     )
     
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=genres_choices
+        choices=State.choices()
     )
     address = StringField(
         'address', validators=[DataRequired()]
@@ -171,10 +101,9 @@ class VenueForm(Form):
         #       raise ValueError(name)
         #return coerce
 #Then add coerce parameter as below:
-
  
         'genres', validators=[DataRequired()],
-          choices= genres_choices  )
+        choices=Genre.choices())
     
     facebook_link = StringField(
         'facebook_link', validators=[Regexp('(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?', message="The correct format for the facebook link was not correct")])
@@ -187,7 +116,22 @@ class VenueForm(Form):
     seeking_description = StringField(
         'seeking_description'
     )
-
+    def validate(self):
+        """Define a custom validate method in your Form:"""
+        rv = Form.validate(self)
+        if not rv:
+            return False
+        if not is_valid_phone(self.phone.data):
+            self.phone.errors.append('Invalid phone.')
+            return False
+        if not set(self.genres.data).issubset(dict(Genre.choices()).keys()):
+            self.genres.errors.append('Invalid genres.')
+            return False
+        if self.state.data not in dict(State.choices()).keys():
+            self.state.errors.append('Invalid state.')
+            return False
+        # if pass validation
+        return True
 
 
 class ArtistForm(Form):
@@ -201,66 +145,14 @@ class ArtistForm(Form):
 
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+        choices=State.choices()
     )
 
      
     phone = StringField(
         # TODO implement validation logic for state
         
-        'phone', validators=[DataRequired(),validate_phone])
+        'phone', validators=[DataRequired(),])
     
     image_link = StringField(
         'image_link'
@@ -268,27 +160,7 @@ class ArtistForm(Form):
 
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
+        choices=Genre.choices()
      )
      
     facebook_link = StringField(
@@ -298,7 +170,7 @@ class ArtistForm(Form):
      
      
     website_link = StringField(
-        'website_link',validators=[Optional(),URL('Invalid URL'),validate_website]
+        'website_link',validators=[Optional(),URL('Invalid URL')]
      )
     
     seeking_venue = BooleanField('seeking_venue')
@@ -307,3 +179,19 @@ class ArtistForm(Form):
             'seeking_description'
      )
 
+    def validate(self):
+        """Define a custom validate method in your Form:"""
+        rv = Form.validate(self)
+        if not rv:
+            return False
+        if not is_valid_phone(self.phone.data):
+            self.phone.errors.append('Invalid phone.')
+            return False
+        if not set(self.genres.data).issubset(dict(Genre.choices()).keys()):
+            self.genres.errors.append('Invalid genres.')
+            return False
+        if self.state.data not in dict(State.choices()).keys():
+            self.state.errors.append('Invalid state.')
+            return False
+        # if pass validation
+        return True
